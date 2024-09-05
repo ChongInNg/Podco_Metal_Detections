16be200ce701b810771f077bad7bdbfdfb340281
chonginng <cn@podco.net>
2024-09-05 18:08:39 -0400

    update network shell

--- network_start.sh
+++ network_start.sh
@@ -1,5 +1,9 @@
 #!/bash/bin
 
+sudo systemctl enable networking.service
+sudo systemctl start networking.service
+echo "start networking.service successfully"
+
 sudo systemctl enable dhcpcd
 sudo systemctl start dhcpcd
 echo "start dhcpcd service successfully."
--- network_stop.sh
+++ network_stop.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+sudo systemctl stop networking.service
+sudo systemctl disable networking.service
+echo "disable networking.service successfully."
+
+
 sudo systemctl stop dhcpcd
 sudo systemctl disable dhcpcd
-
-echo "stop dhcpcd successfully."
+echo "disable dhcpcd successfully."
