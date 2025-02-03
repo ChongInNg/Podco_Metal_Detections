9998edf492ec591a637462dda68b5dbcbc3782e4
chong <cn@podcodesigns.com>
2025-02-03 17:06:57 -0500

    update setting view

--- client/screens/setting_screen.py
+++ client/screens/setting_screen.py
@@ -1,12 +1,15 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, NumericProperty
 from kivy.lang import Builder
 
 Builder.load_file("kv/setting_screen.kv")
 
 class SettingScreen(Screen):
     title = StringProperty('Setting')
+    brightness = NumericProperty(50)
+    bypass_status = NumericProperty(0)
+    bypass_status_value = StringProperty("OFF")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -29,4 +32,16 @@ class SettingScreen(Screen):
         stack_widget.change_to_screen_name("option")
 
     def update_bypass(self, value: int):
-        self.bypass = 1
+        self.bypass = value
+        if self.bypass == 1:
+            self.bypass_status_value = "ON"
+        else:
+            self.bypass_status_value = "OFF"
+        print(f"Update bypass successfully, val: {self.bypass}, {self.bypass_status_value}")
+
+    def on_brightness_change(self, value: int):
+        self.brightness = value
+        print("Update brightness of LCD screen successully.")
+
+    def on_copy_log_click(self):
+        print("Copy log successfully.")
